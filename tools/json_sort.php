708078adc537ae28b20b33d9a1f11b06e0864ac7
korin0912 <>
2021-05-06 00:28:09 +0900

    2021/05/05 分追加 * Movies.json をソート * 不要ファイル削除

---
+++ tools/json_sort.php
@@ -0,0 +1,12 @@
+﻿<?php
+
+$json = json_decode(file_get_contents($argv[1]), true);
+
+function key_cmp($a, $b)
+{
+  return intval($a) - intval($b);
+}
+
+uksort($json, "key_cmp");
+
+file_put_contents($argv[2], json_encode($json, JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES));

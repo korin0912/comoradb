d5d104e34b843a4f6e0ba08c15e19bb5ce5383b3
korin0912 <>
2021-05-08 00:52:10 +0900

    #2: 1動画に複数ゲームあるときに、動画の行を連結

--- src/components/Games.vue
+++ src/components/Games.vue
@@ -24,66 +24,67 @@
             </tr>
           </thead>
           <tbody>
-            <template v-for="(item, itemIndex) in items">
-              <template v-for="(movie, movieIndex) in item.movies">
-                <tr :key="itemIndex + '_' + movieIndex">
-                  <template v-if="movieIndex == 0">
-                    <td
-                      :rowspan="item.movies.length"
-                      class="text-upper text-center left"
-                    >
-                      {{ item.game.id }}
-                    </td>
-                    <td :rowspan="item.movies.length" class="text-upper">
-                      {{ item.game.name }}
-                    </td>
-                    <td
-                      :rowspan="item.movies.length"
-                      class="text-upper text-center"
-                    >
-                      <a
-                        v-for="(url, urlIndex) in item.game.urls"
-                        :key="urlIndex"
-                        :href="url.url"
-                        target="_blank"
-                      >
-                        <i :class="url.icon + ' fa-lg urlicon'" />
-                      </a>
-                    </td>
-                    <td :rowspan="item.movies.length" class="text-upper genres">
-                      <span
-                        v-for="(genre, genreIndex) in item.game.genres"
-                        :key="'genre-' + genreIndex"
-                        class="genre"
-                      >
-                        {{ genre }}
-                      </span>
-                    </td>
-                  </template>
-                  <td class="text-upper text-center">{{ movie.id }}</td>
-                  <td class="text-upper movie">
-                    <a :href="movie.url" target="_blank">{{ movie.name }}</a>
-                  </td>
-                  <td class="text-upper text-center">
-                    {{ movie.releaseDate }}
-                  </td>
-                  <td class="text-upper text-center">
-                    <img
-                      v-for="(actor, actorIndex) in movie.actors"
-                      :key="'actor_' + actorIndex"
-                      :src="require('../assets/actor_' + actor.id + '.png')"
-                      :title="actor.name"
-                      class="actor"
-                    />
-                  </td>
-                  <td>
-                    <div class="text-upper text-center">
-                      <i v-if="movie.chat" class="fas fa-check fa-lg check" />
-                    </div>
-                  </td>
-                </tr>
+            <tr v-for="(item, itemIndex) in items" :key="itemIndex">
+              <!-- ゲーム -->
+              <template v-if="item.game != null">
+                <td :rowspan="item.gameRow" class="text-upper text-center left">
+                  {{ item.game.id }}
+                </td>
+                <td :rowspan="item.gameRow" class="text-upper">
+                  {{ item.game.name }}
+                </td>
+                <td :rowspan="item.gameRow" class="text-upper text-center">
+                  <a
+                    v-for="(url, urlIndex) in item.game.urls"
+                    :key="urlIndex"
+                    :href="url.url"
+                    target="_blank"
+                  >
+                    <i :class="url.icon + ' fa-lg urlicon'" />
+                  </a>
+                </td>
+                <td :rowspan="item.gameRow" class="text-upper genres">
+                  <span
+                    v-for="(genre, genreIndex) in item.game.genres"
+                    :key="'genre-' + genreIndex"
+                    class="genre"
+                  >
+                    {{ genre }}
+                  </span>
+                </td>
               </template>
-            </template>
+              <!-- 動画 -->
+              <template v-if="item.movie != null">
+                <td :rowspan="item.movieRow" class="text-upper text-center">
+                  {{ item.movie.id }}
+                </td>
+                <td :rowspan="item.movieRow" class="text-upper movie">
+                  <a :href="item.movie.url" target="_blank">{{
+                    item.movie.name
+                  }}</a>
+                </td>
+                <td :rowspan="item.movieRow" class="text-upper text-center">
+                  {{ item.movie.releaseDate }}
+                </td>
+                <td :rowspan="item.movieRow" class="text-upper text-center">
+                  <img
+                    v-for="(actor, actorIndex) in item.movie.actors"
+                    :key="'actor_' + actorIndex"
+                    :src="require('../assets/actor_' + actor.id + '.png')"
+                    :title="actor.name"
+                    class="actor"
+                  />
+                </td>
+                <td :rowspan="item.movieRow">
+                  <div class="text-upper text-center">
+                    <i
+                      v-if="item.movie.chat"
+                      class="fas fa-check fa-lg check"
+                    />
+                  </div>
+                </td>
+              </template>
+            </tr>
           </tbody>
         </table>
       </div>
@@ -101,41 +102,40 @@ export default {
   name: "Games",
   data: function () {
     // テーブル要素リスト作成
+    // まず、1行で1ゲーム1動画のリストを作成
+    // そのあとで、連続して重複するゲーム/動画を潰す
+
+    // 1行で1ゲーム1動画のリスト作成
     let items = [];
     Object.keys(gamesData)
       .sort(function (a, b) {
         return Number(a) - Number(b);
       })
-      .forEach((key) => {
-        var game = gamesData[key];
-        let item = {};
+      .forEach((gameId) => {
+        // ゲーム作成
+        var gameData = gamesData[gameId];
+        let game = {
+          id: gameId,
+          name: gameData.name,
+          urls: gameData.urls.map((url) => {
+            return {
+              url: url,
+              icon: get_url_icon_class(get_url_type(url)),
+            };
+          }),
+          genres: gameData.genreIds.map((genreId) => gameGenresData[genreId]),
+        };
 
-        // ゲーム
-        item.game = {};
-        item.game.id = key;
-        item.game.name = game.name;
-        item.game.urls = [];
-        game.urls.forEach((url) => {
-          let type = get_url_type(url);
-          item.game.urls.push({
-            url: url,
-            icon: get_url_icon_class(type),
-          });
-        });
-        item.game.genres = game.genreIds.map(
-          (genreId) => gameGenresData[genreId]
-        );
-
-        // 動画
-        item.movies = [];
+        // ゲームに対応しての動画のリスト作成
+        let isPushItems = false;
         Object.keys(moviesData).forEach((movieId) => {
-          var movie = moviesData[movieId];
-          if (movie.gameIds.find((gameId) => key == gameId) == null) {
+          var movieData = moviesData[movieId];
+          if (movieData.gameIds.find((id) => gameId == id) == null) {
             return;
           }
 
-          let date = movie.releaseDate.split("/");
-          item.movies.push({
+          let date = movieData.releaseDate.split("/");
+          let movie = {
             id: movieId,
             releaseDate:
               date[0] +
@@ -143,32 +143,70 @@ export default {
               ("0" + date[1]).slice(-2) +
               "/" +
               ("0" + date[2]).slice(-2),
-            name: movie.name,
-            url: movie.url,
-            actors: movie.actorIds.map((actorId) => {
+            name: movieData.name,
+            url: movieData.url,
+            actors: movieData.actorIds.map((actorId) => {
               return {
                 id: actorId,
                 name: actorsData[actorId].name,
               };
             }),
-            chat: movie.chat,
+            chat: movieData.chat,
+          };
+
+          items.push({
+            gameRow: 1,
+            game: game,
+            movieRow: 1,
+            movie: movie,
           });
+
+          isPushItems = true;
         });
-        if (item.movies.length <= 0) {
-          // 動画が空はおそらく無いはずだけど、ガードとして
-          item.movies.push({
-            id: "",
-            releaseDate: "",
-            name: "",
-            url: "",
-            actors: "",
-            chat: false,
+
+        // 動画がないことはおそらく無いはずだけど、ガードとして
+        if (!isPushItems) {
+          items.push({
+            gameRow: 1,
+            game: game,
+            movieRow: 1,
+            movie: {
+              id: "",
+              releaseDate: "",
+              name: "",
+              url: "",
+              actors: [],
+              chat: false,
+            },
           });
         }
-
-        // 追加
-        items.push(item);
       });
+
+    // 連続して重複するゲームを潰す
+    let prevItem = null;
+    let prevId = -1;
+    items.forEach((item) => {
+      if (item.game.id != prevId) {
+        prevItem = item;
+        prevId = item.game.id;
+      } else {
+        prevItem.gameRow++;
+        item.game = null;
+      }
+    });
+
+    // 連続して重複する動画を潰す
+    prevItem = null;
+    prevId = -1;
+    items.forEach((item) => {
+      if (item.movie.id != prevId) {
+        prevItem = item;
+        prevId = item.movie.id;
+      } else {
+        prevItem.movieRow++;
+        item.movie = null;
+      }
+    });
 
     // console.log(items);
 

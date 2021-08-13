3733300204c6afb076d589dce8789dbf8f45f0d9
korin0912 <>
2021-08-14 01:02:56 +0900

    カレンダー送り＋モバイル対応

---
+++ src/components/Calendar/MobileTable.vue
@@ -0,0 +1,77 @@
+﻿<template>
+  <table>
+    <tbody>
+      <tr v-for="(day, dayIndex) in days" v-bind:key="`day-${dayIndex}`">
+        <th class="date">{{ day.date }}日</th>
+        <th :class="(day['dayOfWeek'] == 0 || day['dayOfWeek'] == 6 ? 'dark' : 'pale') + ' day-of-week'">{{ dayOfWeeks[day["dayOfWeek"]] }}</th>
+        <td>
+          <div class="day">
+            <div v-for="movie in day.movies" v-bind:key="`movie-${movie.id}`" class="movie">
+              ・<a :href="movie.url">{{ movie.name }}</a>
+            </div>
+          </div>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: "CalendarPCTable",
+  props: ["days"],
+  data: function () {
+    let dayOfWeeks = ["日", "月", "火", "水", "木", "金", "土"];
+    return {
+      dayOfWeeks: dayOfWeeks,
+    };
+  },
+};
+</script>
+
+
+<style scoped>
+tr {
+  height: 40px;
+}
+
+th.date {
+  width: 12%;
+  background-color: var(--comora-color-yellow);
+}
+
+th.day-of-week {
+  width: 8%;
+}
+
+td {
+  vertical-align: middle;
+}
+
+tbody tr:nth-child(1) th {
+  border-top-width: 1px;
+}
+tbody tr:nth-child(1) td {
+  border-top-width: 1px;
+}
+
+div.day {
+  width: 100%;
+}
+
+div.date {
+  width: 100%;
+  text-align: center;
+  background-color: var(--comora-color-yellow);
+  border-radius: 4px;
+  margin: 1px 1px 0px 1px;
+}
+
+div.movie {
+  margin: 1px 1px 0px 1px;
+}
+
+td.hide {
+  background-color: #aaaaaa;
+}
+</style>
--- src/components/Calendar/Show.vue
+++ src/components/Calendar/Show.vue
@@ -2,40 +2,21 @@
   <div>
     <Header />
     <h3>
-      <i class="icon left month-move" />
-      {{ year }}年{{ month }}月
-      <i class="icon right month-move" />
+      <div class="subject">
+        <button v-on:click="moveMonth(-1)" class="move-month"><i class="icon left" /></button>
+        {{ year }}年{{ ("0" + month).slice(-2) }}月
+        <button v-on:click="moveMonth(1)" class="move-month"><i class="icon right" /></button>
+      </div>
     </h3>
-    <table>
-      <thead>
-        <tr>
-          <th class="dark">日</th>
-          <th class="pale">月</th>
-          <th class="pale">火</th>
-          <th class="pale">水</th>
-          <th class="pale">木</th>
-          <th class="pale">金</th>
-          <th class="dark">土</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(week, weekIndex) in weeks" v-bind:key="`week-${weekIndex}`">
-          <td v-for="(day, dayIndex) in week" v-bind:key="`day-${weekIndex}-${dayIndex}`" :class="day.date != -1 ? '' : 'hide'">
-            <div v-if="day.date != -1" class="day">
-              <div class="date">{{ day.date }}日</div>
-              <div v-for="movie in day.movies" v-bind:key="`movie-${movie.id}`" class="movie">
-                ・<a :href="movie.url">{{ movie.name }}</a>
-              </div>
-            </div>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <PCTable v-if="isMobile == false" :weeks="weeks" />
+    <MobileTable v-else :days="days" />
   </div>
 </template>
 
 <script>
 import Header from "../Common/Header.vue";
+import PCTable from "./PCTable.vue";
+import MobileTable from "./MobileTable.vue";
 
 import common from "../Common/Common.js";
 import resources from "../Common/Resources.js";
@@ -44,109 +25,134 @@ export default {
   name: "CalendarShow",
   components: {
     Header,
+    PCTable,
+    MobileTable,
   },
   data: function () {
     // console.clear();
     let year = this.$route.params.year;
     let month = this.$route.params.month;
-
-    // 日次データ作成
-    let days = [];
-
-    // 日曜を開始日として、前月の日をダミーとして追加
-    let firstDate = new Date(year, month - 1, 1);
-    let firstDay = firstDate.getDay();
-    for (let i = 0; i < firstDay; i++) {
-      days.push(-1);
-    }
-
-    // 月末日までを追加
-    let lastDate = new Date(year, month, 0);
-    for (let i = 1; i <= lastDate.getDate(); i++) {
-      days.push(i);
-    }
-
-    // 土曜日を終了日として、翌月の日をダミーとして追加
-    for (let i = 0; days.length % 7 != 0; i++) {
-      days.push(-1);
-    }
-
-    // 日次 ⇒ 週次データ作成
-    let weeks = [];
-    let moviesData = resources.getMoviesData();
-
-    for (let i = 0; i < days.length / 7; i++) {
-      let week = [];
-      for (let j = 0; j < 7; j++) {
-        let date = days[i * 7 + j];
-        let day = {
-          date: -1,
-          movies: [],
-        };
-        if (date != -1) {
-          day["date"] = date;
-          let releaseDate = year + "/" + ('0' + month).slice(-2) + "/" + ('0' + date).slice(-2);
-          console.log(releaseDate);
-          Object.keys(moviesData).forEach((movieId) => {
-            let movieData = moviesData[movieId];
-            if (movieData.releaseDate == releaseDate) {
-              day["movies"].push({
-                id: movieId,
-                name: movieData.name,
-                url: movieData.url,
-              });
-            }
-          });
-        }
-        week.push(day);
-      }
-      weeks.push(week);
-    }
+    let days = makeDays(year, month);
+    let weeks = makeWeeks(days);
 
     return {
       isMobile: common.isMobile(),
       year: year,
       month: month,
+      days: days,
       weeks: weeks,
     };
+  },
+  methods: {
+    moveMonth: function (move) {
+      this.month = parseInt(this.month) + parseInt(move);
+      if (this.month <= 0) {
+        this.year = parseInt(this.year) - 1;
+        this.month = 12;
+      }
+      if (this.month > 12) {
+        this.year = parseInt(this.year) + 1;
+        this.month = 1;
+      }
+      this.days = makeDays(this.year, this.month);
+      this.weeks = makeWeeks(this.days);
+    },
   },
   mounted: async function () {
     await resources.execute();
   },
 };
+
+// 日次データ作成
+function makeDays(year, month) {
+  let ret = [];
+  let moviesData = resources.getMoviesData();
+
+  // 月末日までを追加
+  let firstDate = new Date(year, month - 1, 1);
+  let lastDate = new Date(year, month, 0);
+  for (let i = 1; i <= lastDate.getDate(); i++) {
+    let day = {
+      date: i,
+      dayOfWeek: (firstDate.getDay() + i - 1) % 7,
+      movies: [],
+    };
+    let releaseDate = year + "/" + ("0" + month).slice(-2) + "/" + ("0" + i).slice(-2);
+    // console.log(releaseDate);
+    Object.keys(moviesData).forEach((movieId) => {
+      let movieData = moviesData[movieId];
+      if (movieData.releaseDate == releaseDate) {
+        day["movies"].push({
+          id: movieId,
+          name: movieData.name,
+          url: movieData.url,
+        });
+      }
+    });
+    ret.push(day);
+  }
+
+  return ret;
+}
+
+// 週次データ作成
+function makeWeeks(days) {
+  days = common.copyDeep(days);
+
+  // 日曜を開始日として、前月の日をダミーとして追加
+  let firstDayOfWeek = days[0]["dayOfWeek"];
+  for (let i = 0; i < firstDayOfWeek; i++) {
+    days.unshift({
+      date: -1,
+      dayOfWeek: i,
+      movies: [],
+    });
+  }
+
+  // 土曜日を終了日として、翌月の日をダミーとして追加
+  let lastDayOfWeek = days[days.length - 1]["dayOfWeek"];
+  for (let i = lastDayOfWeek + 1; i < 7; i++) {
+    days.push({
+      date: -1,
+      dayOfWeek: i,
+      movies: [],
+    });
+  }
+
+  // 日次 ⇒ 週次データ作成
+  let ret = [];
+
+  for (let i = 0; i < days.length / 7; i++) {
+    let week = [];
+    for (let j = 0; j < 7; j++) {
+      week.push(days[i * 7 + j]);
+    }
+    ret.push(week);
+  }
+
+  return ret;
+}
 </script>
 
 <style scoped>
-th {
-  width: 14%;
+div.subject {
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
-td {
-  height: 100px;
-  vertical-align: text-top;
-}
-
-div.day {
-  width: 100%;
-}
-
-div.date {
-  width: 100%;
-  text-align: center;
-  background-color: var(--comora-color-yellow);
-  border-radius: 4px;
-  margin: 1px 1px 0px 1px;
-}
-
-div.movie {
-  margin: 1px 1px 0px 1px;
-}
-
-td.hide {
-  background-color: #aaaaaa;
-}
-
-i.month-move {
+h3 {
+  margin-bottom: 10px;
   vertical-align: middle;
+}
+
+button.move-month {
+  border-width: 1px;
+  border-style: solid;
+  border-color: var(--comora-color-dirk-purple);
+  border-radius: 8px;
+  padding: 2px 8px 2px 8px;
+  background-color: white;
+  margin: 0 6px 0 4px;
 }
 </style>
